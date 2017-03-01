748d8ddd58de3f47c508022de88b2f77d953a818
Deep <deep7176@gmail.com>
2017-03-01 12:37:52 -0500

    Initial commit

---
+++ app/src/main/java/com/stuff/app/learningandroid/Appbase.java
@@ -0,0 +1,15 @@
+package com.stuff.app.learningandroid;
+
+import android.app.Application;
+
+/**
+ * Created by deep on 3/1/17.
+ */
+public class Appbase extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //seting up sdk's and other app global things...
+    }
+}
